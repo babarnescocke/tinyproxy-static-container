59da54c796216fea2a4b494bcdd35f3c20788d74
Brian Barnes-Cocke <brian@barnescocke.com>
2024-04-30 15:43:06 -0700

    git works, latest wip

--- tinyproxy-static-container.sh
+++ tinyproxy-static-container.sh
@@ -1,41 +1,48 @@
 #!/bin/sh
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 build0=$(buildah from alpine:3)
-build0mnt=$(buildah mount "$build0")
 build1=$(buildah from scratch)
+
+# Mount both build containers
+build0mnt=$(buildah mount "$build0")
 build1mnt=$(buildah mount "$build1")
-# Provide logic to build either git or latest release
 
-if [[ "$#" -eq 0 || "${1^^}" == "LATEST" ]]
-then
-buildah run "$build0" sh -c 'apk add -q --no-cache automake linux-headers alpine-sdk libtool pcre-dev zlib-dev autoconf &&\
-  git clone https://github.com/tinyproxy/tinyproxy.git &&\
-  cd tinyproxy;
-  ./autogen.sh;
-  LDFLAGS="-static" ./configure;
-  make;
-  make install;
-  strip /local/usr/bin/tinyproxy'
-cp $build0mnt/local/usr/bin/tinyproxy $build1mnt
-buildah unmount $build1mnt $build0mnt
-buildah config --entrypoint '[ "tinyproxy","-d","-c","/config.txt"]' \
- --workingdir /config "$build1"
-buildah commit --squash --disable-compression=false --rm "$build1" tinyproxy-scratch-container
+# Common preparation: installing dependencies
+buildah run "$build0" sh -c 'apk add -q --no-cache automake linux-headers alpine-sdk libtool pcre-dev zlib-dev autoconf curl  jq'
+
+# Function to configure, build, and install Tinyproxy
+build_and_install() {
+  buildah run "$build0" sh -c "cd /tmp/tinyproxy; \
+    ./autogen.sh; \
+    LDFLAGS='-static' ./configure; \
+    make; \
+    make install; \
+    strip /usr/local/bin/tinyproxy"
+}
+
+if [ "$#" -eq 0 ] || [ "${1^^}" != "GIT" ]; then
+  # Download and build from the latest release tarball
+  REPO="tinyproxy/tinyproxy"
+  LATEST_RELEASE=$(curl -sL "https://api.github.com/repos/$REPO/releases/latest")
+  TARBALL_URL=$(echo $LATEST_RELEASE | jq -r '.assets[] | select(.name | endswith(".tar.gz")) | .browser_download_url' )
+  buildah run "$build0" sh -c "curl -L $TARBALL_URL | tar zx -C /tmp; \
+    DIR_NAME=$(ls /tmp | grep -m 1 tinyproxy); \
+    cd /tmp/\$DIR_NAME"
+  build_and_install
+  IMAGE_TAG="tinyproxy-scratch-container:latest"
 else
-buildah run "$build0" sh -c 'apk add -q --no-cache automake linux-headers alpine-sdk libtool pcre-dev zlib-dev autoconf curl; \
-  REPO="tinyproxy/tinyproxy"; \
-  LATEST_RELEASE=$(curl -s "https://api.github.com/repos/$REPO/releases/latest"); \
-  TARBALL_URL=$(echo $LATEST_RELEASE | grep tarball_url | cut -d '"' -f 4); \
-  curl -L $TARBALL_URL | tar zx; \
-  cd tinyproxy; \
-  ./autogen.sh;
-  LDFLAGS="-static" ./configure;
-  make;
-  make install;
-  strip /local/usr/bin/tinyproxy'
-cp $build0mnt/local/usr/bin/tinyproxy $build1mnt
-buildah unmount $build1mnt $build0mnt
-buildah config --entrypoint '[ "tinyproxy","-d","-c","/config.txt"]' \
- --workingdir /config "$build1"
-buildah commit --squash --disable-compression=false --rm "$build1" tinyproxy-scratch-container
-
+  # Clone and build from the Git repository
+  buildah run "$build0" sh -c 'git clone https://github.com/tinyproxy/tinyproxy.git /tmp/tinyproxy'
+  build_and_install
+  IMAGE_TAG="tinyproxy-scratch-container:git"
 fi
+
+# Copy the binary to the scratch image and unmount
+cp $build0mnt/usr/local/bin/tinyproxy $build1mnt/tinyproxy
+buildah unmount "$build0" "$build1"  # Use container names here
+
+# Set the entrypoint and working directory
+buildah config --entrypoint '["tinyproxy", "-d", "-c", "/config.txt"]' --workingdir /config "$build1"
+buildah commit --squash --disable-compression=false --rm "$build1" "$IMAGE_TAG"
+
